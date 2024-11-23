9e0369ad1923973aa50c583b82995a8a7d7e29a7
Holland Gibson <holland@hollandgibson.com>
2024-11-23 16:01:50 -0500

    Add the wish-list article and some tooling improvements

---
+++ bin/resize_img.sh
@@ -0,0 +1,2 @@
+magick 20250130-dan-cristian-padure-h3kuhYUCE9A-unsplash.jpg -sampling-factor 4:2:0 -strip -qual
+ity 85 -interlace JPEG -colorspace RGB -resize 'x1200>' 20250130-dan-cristian-padure-h3kuhYUCE9A-unsplash.min.jpg
